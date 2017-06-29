ffa2b892b8c0de1abfbd2df8de0185c24a3dc2b0
Tamas Boros <tamas_boros@epam.com>
2017-06-29 16:11:35 +0200

    first commit

---
+++ src/main/java/com/geocachestore/main/GeoCacheStoreMainProjectApplication.java
@@ -0,0 +1,12 @@
+package com.geocachestore.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoCacheStoreMainProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoCacheStoreMainProjectApplication.class, args);
+	}
+}
